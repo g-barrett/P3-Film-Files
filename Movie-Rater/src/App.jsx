c3b4de94cb7daf6d50b394d0efb741c6c12a4c92
Daniel Park <danielapark87@gmail.com>
2024-01-27 23:30:15 -0600

    Created home.jsx and movie.jsx

--- Movie-Rater/src/App.jsx
+++ Movie-Rater/src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
+import { useState, React, createContext, useContext } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
+
   const [count, setCount] = useState(0)
 
   return (
