b8e3951eabdd863d40b9e5e1b39cf2afb24b552a
Daniel Park <danielapark87@gmail.com>
2024-01-29 21:42:35 -0600

    updated schema and typedefs to match client side

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,6 @@
 const typeDefs = `
 type User {
     _id: ID
-    firstName: String
-    lastName: String
     email: String
     reviews: [Review]
   }
@@ -40,7 +38,7 @@ type User {
   }
 
   type Mutation {
-    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    addUser(email: String!, password: String!): Auth
     updateUser(firstName: String, lastName: String, email: String, password: String): User
     login(email: String!, password: String!): Auth
     updateMovie(movieId: ID!, review: String!): Movie 
