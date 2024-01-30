c5a9f39a763f7dcaeb76b27bb29ac35f9ac8ddb0
Cara R <cara.rosa.02@gmail.com>
2024-01-30 12:57:21 -0600

    debugging movies.jsx file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,9 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App.jsx';
 import Error from './pages/Error.jsx';
-import AddProfile from './pages/AddProfile.jsx';
-import Signup from './pages/Signup.jsx';
-import Login from './pages/Login.jsx';
+// import AddProfile from './pages/AddProfile.jsx';
+// import Signup from './pages/Signup.jsx';
+// import Login from './pages/Login.jsx';
 import Movie from './pages/Movie.jsx';
 import Review from './pages/Review.jsx';
 // import Signup from './pages/Signup.jsx';
@@ -24,30 +24,30 @@ const router = createBrowserRouter([
        index: true,
        element: <Movie />
       },
-      {
-       path: '/login',
-       element: <Login />
-     },
-      {
-        path: '/profiles/:profileId',
-        element: <AddProfile />
-      },
-      {
-        path: '/movies',
-        element: <AddProfile />
-      },
-      {
-        path: '/movies',
-        element: <Movie />
-      },
+    //   {
+    //    path: '/login',
+    //    element: <Login />
+    //  },
+      // {
+      //   path: '/profiles/:profileId',
+      //   element: <AddProfile />
+      // },
+      // {
+      //   path: '/movies',
+      //   element: <AddProfile />
+      // },
+      // {
+      //   path: '/movies',
+      //   element: <Movie />
+      // },
       {
         path: '/review',
         element: <Review />
       },
-      {
-        path: '/signup',
-        element: <Signup />
-      }
+      // {
+      //   path: '/signup',
+      //   element: <Signup />
+      // }
     ]
   },
 ]);
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,159 +1,159 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-// import { ADD_PROFILE } from '../utils/mutations';
-import { LOGIN } from '../utils/mutations';
-import Auth from '../utils/auth';
+// import { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import { useMutation } from '@apollo/client';
+// // import { ADD_PROFILE } from '../utils/mutations';
+// import { LOGIN } from '../utils/mutations';
+// import Auth from '../utils/auth';
 
-// const Login = () => {
-//     const [formState, setFormState] = useState({
-//         email: '',
-//         password: '',
-//     });
+// // const Login = () => {
+// //     const [formState, setFormState] = useState({
+// //         email: '',
+// //         password: '',
+// //     });
 
-//     const [login, { error, data}] = useMutation(LOGIN);
+// //     const [login, { error, data}] = useMutation(LOGIN);
 
-//     const handleChange = (event) => {
-//         const { name, value } = event.target;
+// //     const handleChange = (event) => {
+// //         const { name, value } = event.target;
 
-//         setFormState({
-//             ...formState,
-//             [name]: value,
-//         });
-//     };
+// //         setFormState({
+// //             ...formState,
+// //             [name]: value,
+// //         });
+// //     };
 
-//     // form that submits
+// //     // form that submits
+// //     const handleFormSubmit = async (event) => {
+// //         event.preventDefault();
+
+// //         try {
+// //             const { data } = await login({
+// //                 variables: { ...formState },
+// //             });
+
+// //             Auth.login(data.login.token);
+// //         } catch (e) {
+// //             console.error(e);
+// //         }
+// //     };
+
+// function Login(props) {
+//     const [formState, setFormState] = useState({ email: '', password: '' });
+//     const [login, { error, data }] = useMutation(LOGIN);
+  
 //     const handleFormSubmit = async (event) => {
-//         event.preventDefault();
-
-//         try {
-//             const { data } = await login({
-//                 variables: { ...formState },
-//             });
-
-//             Auth.login(data.login.token);
-//         } catch (e) {
-//             console.error(e);
-//         }
+//       event.preventDefault();
+//       try {
+//         const mutationResponse = await login({
+//           variables: { email: formState.email, password: formState.password },
+//         });
+//         const token = mutationResponse.data.login.token;
+//         Auth.login(token);
+//       } catch (e) {
+//         console.log(e);
+//       }
 //     };
-
-function Login(props) {
-    const [formState, setFormState] = useState({ email: '', password: '' });
-    const [login, { error, data }] = useMutation(LOGIN);
   
-    const handleFormSubmit = async (event) => {
-      event.preventDefault();
-      try {
-        const mutationResponse = await login({
-          variables: { email: formState.email, password: formState.password },
-        });
-        const token = mutationResponse.data.login.token;
-        Auth.login(token);
-      } catch (e) {
-        console.log(e);
-      }
-    };
-  
-    // const handleChange = (event) => {
-    //   const { name, value } = event.target;
-    //   setFormState({
-    //     ...formState,
-    //     [name]: value,
-    //   });
-    // };
+//     // const handleChange = (event) => {
+//     //   const { name, value } = event.target;
+//     //   setFormState({
+//     //     ...formState,
+//     //     [name]: value,
+//     //   });
+//     // };
 
-//     return (
-//         <main className="flex-row justify-center mb-4">
-//             <div className="col-12 col-lg-10">
-//                 <div className="card">
-//                     <h4 className="card-header bg-dark text-light p-2">Sign Up</h4>
-//                     <div className="card-two">
-//                         {data ? (
-//                             <p>Logged in successfully!</p>
-//                         ): (
-//                             <form onSubmit={handleFormSubmit}>
-//                                 <input
-//                                 className="form"
-//                                 placeholder="Email"
-//                                 name="email"
-//                                 type="email"
-//                                 value={formState.email}
-//                                 onChange={handleChange}
-//                                 />
-//                                 <input
-//                                 className="form"
-//                                 placeholder="*****"
-//                                 name="password"
-//                                 type="password"
-//                                 value={formState.password}
-//                                 onChange={handleChange}
-//                                 />
-//                                 <button
-//                                 className="button btn-block btn-info"
-//                                 style={{ cursor: 'pointer'}}
-//                                 type="submit"
-//                                 >
-//                                     Submit
-//                                 </button>
-//                             </form>
-//                         )}
-//                         {error && (
-//                             <div className="error p-3 bg-danger text-white">
-//                                 {error.message}
-//                             </div>
-//                         )}
-//                     </div>
-//                 </div>
-//             </div>
-//         </main>
-//     );
-// };
+// //     return (
+// //         <main className="flex-row justify-center mb-4">
+// //             <div className="col-12 col-lg-10">
+// //                 <div className="card">
+// //                     <h4 className="card-header bg-dark text-light p-2">Sign Up</h4>
+// //                     <div className="card-two">
+// //                         {data ? (
+// //                             <p>Logged in successfully!</p>
+// //                         ): (
+// //                             <form onSubmit={handleFormSubmit}>
+// //                                 <input
+// //                                 className="form"
+// //                                 placeholder="Email"
+// //                                 name="email"
+// //                                 type="email"
+// //                                 value={formState.email}
+// //                                 onChange={handleChange}
+// //                                 />
+// //                                 <input
+// //                                 className="form"
+// //                                 placeholder="*****"
+// //                                 name="password"
+// //                                 type="password"
+// //                                 value={formState.password}
+// //                                 onChange={handleChange}
+// //                                 />
+// //                                 <button
+// //                                 className="button btn-block btn-info"
+// //                                 style={{ cursor: 'pointer'}}
+// //                                 type="submit"
+// //                                 >
+// //                                     Submit
+// //                                 </button>
+// //                             </form>
+// //                         )}
+// //                         {error && (
+// //                             <div className="error p-3 bg-danger text-white">
+// //                                 {error.message}
+// //                             </div>
+// //                         )}
+// //                     </div>
+// //                 </div>
+// //             </div>
+// //         </main>
+// //     );
+// // };
 
-const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
+// const handleChange = (event) => {
+//     const { name, value } = event.target;
+//     setFormState({
+//       ...formState,
+//       [name]: value,
+//     });
+//   };
 
-  return (
-    <div className="container my-1">
-      <Link to="/signup">← Go to Signup</Link>
+//   return (
+//     <div className="container my-1">
+//       <Link to="/signup">← Go to Signup</Link>
 
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        {error ? (
-          <div>
-            <p className="error-text">The provided credentials are incorrect</p>
-          </div>
-        ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
-  );
-}
+//       <h2>Login</h2>
+//       <form onSubmit={handleFormSubmit}>
+//         <div className="flex-row space-between my-2">
+//           <label htmlFor="email">Email address:</label>
+//           <input
+//             placeholder="youremail@test.com"
+//             name="email"
+//             type="email"
+//             id="email"
+//             onChange={handleChange}
+//           />
+//         </div>
+//         <div className="flex-row space-between my-2">
+//           <label htmlFor="pwd">Password:</label>
+//           <input
+//             placeholder="******"
+//             name="password"
+//             type="password"
+//             id="pwd"
+//             onChange={handleChange}
+//           />
+//         </div>
+//         {error ? (
+//           <div>
+//             <p className="error-text">The provided credentials are incorrect</p>
+//           </div>
+//         ) : null}
+//         <div className="flex-row flex-end">
+//           <button type="submit">Submit</button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// }
 
-export default Login;
+// export default Login;
